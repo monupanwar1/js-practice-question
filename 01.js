ba21b1ad9b29bc0ed5fd031a4faf2ff99cb4b8b8
kunal <monupanwar5655@gmail.com>
2025-03-22 15:11:42 +0530

    ques 1 convert string to json and vice-versa

---
+++ 01.js
@@ -0,0 +1,15 @@
+const user ={
+    name:"kunalPanwar",
+    age:20,
+    profession:"software enginner",
+    company:"Microsoft"
+}
+// convert obj to String ✅;
+const jsonString=JSON.stringify(user);
+console.log(jsonString);
+
+// convert string to object
+const jsonObject = JSON.parse('{"name":"kunalPanwar","age":20,"profession":"software engineer","company":"Microsoft"}');
+console.log(jsonObject)
+
+
