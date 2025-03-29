3c2a457c97c502dc712e62aba826955f00e4eb53
kunal <monupanwar5655@gmail.com>
2025-03-29 17:23:27 +0530

    tru vs false

---
+++ 28.js
@@ -0,0 +1,12 @@
+// falsy value
+if(0) console.log(0)
+if('') console.log('empty')
+if(null) console.log(null)
+if(undefined) console.log(undefined)
+
+    // truhty value
+if("heheh") console.log("hehehe")
+if([]) console.log("hehehe")
+if({}) console.log("hehehe")
+if(42) console.log("42")
+    
